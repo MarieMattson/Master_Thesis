e99ccd43354c2783bc298b3dc8d6eee165c7ea6e
MarieMattson <marie.mattson18@gmail.com>
2025-03-05 09:03:32 +0100

    started creating test set generation

---
+++ llm_retrieval/evaluation/generate_test_set.py
@@ -0,0 +1,102 @@
+import random
+from tqdm.auto import tqdm
+import pandas as pd
+from typing import Optional, List, Tuple
+import json
+import datasets
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document as LangchainDocument
+from huggingface_hub import notebook_login
+from huggingface_hub import InferenceClient
+
+from datasets import load_dataset
+
+# Load dataset from a directory containing JSON files
+dataset = load_dataset("json", data_files="/mnt/c/Users/User/thesis/data_import/filtered_riksdag.json")
+dataset = dataset.filter(lambda x: x["dok_id"] == "H00998")
+
+pd.set_option("display.max_colwidth", None)
+notebook_login()
+#ds = datasets.load_dataset("m-ric/huggingface_doc", split="train")
+langchain_docs = [
+    LangchainDocument(page_content=doc["anforandetext"], metadata={"source": doc["dok_id"]}) 
+    for doc in tqdm(dataset["train"])
+]
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=200,
+    add_start_index=True,
+    separators=["\n\n", "\n", ".", " ", ""],
+)
+
+docs_processed = []
+for doc in langchain_docs:
+    docs_processed += text_splitter.split_documents([doc])
+
+repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+
+llm_client = InferenceClient(
+    model=repo_id,
+    timeout=120,
+)
+
+
+def call_llm(inference_client: InferenceClient, prompt: str):
+    response = inference_client.post(
+        json={
+            "inputs": prompt,
+            "parameters": {"max_new_tokens": 1000},
+            "task": "text-generation",
+        },
+    )
+    return json.loads(response.decode())[0]["generated_text"]
+
+
+call_llm(llm_client, "This is a test context")
+
+QA_generation_prompt = """
+Du måste generera frågor och svar på svenska.
+Frågorna ska röra innehållet i debatten. 
+Exempelvis frågor om vad olika politiker tycker om sakfrågor som diskuteras.
+Ett exempel kan vara "Vad påstår Anders Borg om restuarangmoms?".
+Your task is to write a factoid question and an answer given a context.
+Your factoid question should be answerable with a specific, concise piece of factual information from the context.
+Your factoid question should be formulated in the same style as questions users could ask in a search engine.
+This means that your factoid question MUST NOT mention something like "according to the passage" or "context".
+
+Provide your answer as follows:
+
+Output:::
+Factoid question: (your factoid question)
+Answer: (your answer to the factoid question)
+
+Now here is the context.
+
+Context: {context}\n
+Output:::"""
+
+N_GENERATIONS = 10  # We intentionally generate only 10 QA couples here for cost and time considerations
+
+print(f"Generating {N_GENERATIONS} QA couples...")
+
+outputs = []
+for sampled_context in tqdm(random.sample(docs_processed, N_GENERATIONS)):
+    # Generate QA couple
+    output_QA_couple = call_llm(llm_client, QA_generation_prompt.format(context=sampled_context.page_content))
+    try:
+        question = output_QA_couple.split("Factoid question: ")[-1].split("Answer: ")[0]
+        answer = output_QA_couple.split("Answer: ")[-1]
+        assert len(answer) < 300, "Answer is too long"
+        outputs.append(
+            {
+                "context": sampled_context.page_content,
+                "question": question,
+                "answer": answer,
+                "source_doc": sampled_context.metadata["source"],
+            }
+        )
+    except:
+        continue
+
+print(outputs)
