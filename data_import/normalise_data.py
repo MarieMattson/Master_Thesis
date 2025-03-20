ea84c84e085f4ef8d656710955fbc7bc6dc2207a
MarieMattson <marie.mattson18@gmail.com>
2025-03-20 15:07:01 +0100

    created and checked a test set which looks pretty good

--- data_import/normalise_data.py
+++ data_import/normalise_data.py
@@ -4,15 +4,16 @@ import re
 
 class Speaker(BaseModel):
     talare: str
+    parti: str
 
     @field_validator('talare')
     def clean_talare(cls, value):
-        # List of titles to remove
+        # List of titles to remove from 'talare'
         titles = [
             "Statsrådet", "Utbildningsminister", "Kultur- och idrottsminister",
             "Socialminister", "Finansminister", "Justitieminister", "Försvarsminister",
             "Minister", "Kulturministern", "Justitie- och inrikesministern", "Statsministern", 
-        "Infrastrukturministern", "Klimat- och miljöministern", "Näringsministern"]
+            "Infrastrukturministern", "Klimat- och miljöministern", "Näringsministern"]
         
         # Remove any title at the start of the name
         pattern = r'^(?:' + '|'.join(titles) + r')\s+'
@@ -26,15 +27,22 @@ class Speaker(BaseModel):
 
         return value
 
-input_file = "/mnt/c/Users/User/thesis/data_import/exp1/filtered_riksdag_exp1.json"
-output_file = "/mnt/c/Users/User/thesis/data_import/exp1/filtered_riksdag_exp1.json"  
+    @field_validator('parti')
+    def clean_parti(cls, value):
+        # Remove the party affiliation (inside parentheses)
+        value = re.sub(r'\s*\(.*\)$', '', value)
+        return value
+
+input_file = "/mnt/c/Users/User/thesis/data_import/filtered_riksdag.json"
+output_file = "/mnt/c/Users/User/thesis/data_import/filtered_riksdag.json"  
 
 with open(input_file, 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 for item in data:
     if 'talare' in item:
-        item['talare'] = Speaker(**item).talare 
+        # Clean 'talare' and 'parti' fields using Speaker model
+        item['talare'] = Speaker(**item).talare
 
 with open(output_file, 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=4, ensure_ascii=False)
